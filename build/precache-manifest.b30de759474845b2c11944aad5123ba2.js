720c6c36255881447ab8625e007b53e26c4607b4
Grimm <repisk@gmail.com>
2021-01-18 17:20:29 +0500

    дезайн

---
+++ build/precache-manifest.b30de759474845b2c11944aad5123ba2.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9514d97f26b4290c425e9ef26ae2b963",
+    "url": "/Portfolio/index.html"
+  },
+  {
+    "revision": "766b9571f783d8e528df",
+    "url": "/Portfolio/static/css/main.46b2e431.chunk.css"
+  },
+  {
+    "revision": "7faa5d86f94ecc60ea51",
+    "url": "/Portfolio/static/js/2.a7af66e7.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/Portfolio/static/js/2.a7af66e7.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "766b9571f783d8e528df",
+    "url": "/Portfolio/static/js/main.a44dda5f.chunk.js"
+  },
+  {
+    "revision": "b83a37198ba343383876",
+    "url": "/Portfolio/static/js/runtime-main.e440ee5a.js"
+  }
+]);
